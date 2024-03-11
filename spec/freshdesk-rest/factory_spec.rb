ad68d81d82e99c11e2ab21e410b8a34de76b887c
Artem Boltunov <artem.boltunov@helpling.com>
2024-03-11 12:04:21 +0000

    Add spec

--- spec/freshdesk-rest/factory_spec.rb
+++ spec/freshdesk-rest/factory_spec.rb
@@ -2,47 +2,80 @@ require_relative '../../lib/freshdesk-rest'
 
 RSpec.describe Freshdesk::Rest::Factory do
   let(:service) { described_class }
-  let(:config)  { double('Freshdesk::Rest::Configuration') }
-  before do
-    Freshdesk::Rest.configure do |config|
-      config.api_key = 'SOME_FRESHDESK_API_KEY'
-      config.domain = 'SOME_FRESHDESK_DOMAIN'
+
+  context 'when main account is configured' do
+    before do
+      Freshdesk::Rest.configure do |config|
+        config.api_key = 'SOME_FRESHDESK_API_KEY'
+        config.domain = 'SOME_FRESHDESK_DOMAIN'
+      end
+    end
+
+    describe '.api' do
+      subject { service.api }
+
+      it {
+        allow(RestClient::Resource).to receive(:new)
+        subject.send(:resource, '/test_path')
+        is_expected.to be_a(Freshdesk::Rest::Api)
+        expect(RestClient::Resource).to have_received(:new).with(
+          "https://SOME_FRESHDESK_DOMAIN.freshdesk.com/api/v2/test_path",
+          "SOME_FRESHDESK_API_KEY",
+          "X"
+        )
+      }
+    end
+
+    describe '.contact_resource' do
+      subject { service.contact_resource }
+
+      it { is_expected.to be_a(Freshdesk::Rest::Resource::Contact) }
+    end
+
+    describe '.ticket_resource' do
+      subject { service.ticket_resource }
+
+      it { is_expected.to be_a(Freshdesk::Rest::Resource::Ticket) }
+    end
+
+    describe '.solutions_article_resource' do
+      subject { service.solutions_article_resource }
+
+      it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Article) }
+    end
+
+    describe '.solutions_category_resource' do
+      subject { service.solutions_category_resource }
+
+      it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Category) }
+    end
+
+    describe '.solutions_folder_resource' do
+      subject { service.solutions_folder_resource }
+
+      it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Folder) }
     end
   end
 
-  describe '.api' do
-    subject { service.api }
+  context 'when second account is configured' do
+    Freshdesk::Rest.configure(:second_account) do |config|
+      config.api_key = 'SECOND_FRESHDESK_API_KEY'
+      config.domain = 'SECOND_FRESHDESK_DOMAIN'
+    end
 
-    it { is_expected.to be_a(Freshdesk::Rest::Api) }
-  end
+    describe '.api' do
+      subject { service.api(:second_account) }
 
-  describe '.contact_resource' do
-    subject { service.contact_resource }
-
-    it { is_expected.to be_a(Freshdesk::Rest::Resource::Contact) }
-  end
-
-  describe '.ticket_resource' do
-    subject { service.ticket_resource }
-
-    it { is_expected.to be_a(Freshdesk::Rest::Resource::Ticket) }
-  end
-
-  describe '.solutions_article_resource' do
-    subject { service.solutions_article_resource }
-
-    it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Article) }
-  end
-
-  describe '.solutions_category_resource' do
-    subject { service.solutions_category_resource }
-
-    it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Category) }
-  end
-
-  describe '.solutions_folder_resource' do
-    subject { service.solutions_folder_resource }
-
-    it { is_expected.to be_a(Freshdesk::Rest::Resource::Solutions::Folder) }
+      it {
+        allow(RestClient::Resource).to receive(:new)
+        subject.send(:resource, '/test_path')
+        is_expected.to be_a(Freshdesk::Rest::Api)
+        expect(RestClient::Resource).to have_received(:new).with(
+          "https://SECOND_FRESHDESK_DOMAIN.freshdesk.com/api/v2/test_path",
+          "SECOND_FRESHDESK_API_KEY",
+          "X"
+        )
+      }
+    end
   end
 end
